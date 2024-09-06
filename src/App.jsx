99b27206f421f062d85dafcbb2f9010abfb9b019
Farda Ramdhan <fardazza@gmail.com>
2024-09-06 14:14:08 +0700

    delete component

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ import SugestOrphanagePage from "./pages/orphanage/SugestOrphanagePage";
 import CheckDonation from "./pages/donor/CheckDonation";
 import ListUsers from "./pages/admin/ListUsers";
 import ListOrphanages from "./pages/admin/ListOrphanages";
-// import AddOrphanages from "./pages/admin/AddOrphanages";
 import ConfirmBooks from "./pages/admin/ConfirmBooks";
 
 
@@ -29,7 +28,6 @@ function App() {
         <Route path="/admin-dashboard" element={<HomePage />} />
         <Route path="/admin-ListUsers" element={<ListUsers />} />
         <Route path="/admin-ListOrphanage" element={<ListOrphanages />} />
-        {/* <Route path="/admin-AddOrphanage" element={<AddOrphanages />} /> */}
         <Route path="/admin-confirmBook" element={<ConfirmBooks />} />
         <Route path="/login" element={<AuthPage />} />
         <Route path="/donatur-signup" element={<DonaturSignUp />} />
